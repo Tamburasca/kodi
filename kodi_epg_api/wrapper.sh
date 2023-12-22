df3ab73ba35c78f5a307b177456ef7daba128bd1
Tamburasca <rtimmermann@astro.uni-bonn.de>
2023-12-22 16:37:06 +0100

    version 0.2.0 debug and epg_cached can be modified as bool in .env

--- kodi_epg_api/wrapper.sh
+++ kodi_epg_api/wrapper.sh
@@ -7,14 +7,13 @@ cd /iptv/iptv/src/ || exit
 python3 -u webAPI_IPTV_SimpleClient.py --iptv_url "${IPTV_URL}" ${debug} ${epg_cached} &
 
 cd /iptv/epg/scripts/commands/epg/ || exit
-# Rest API throws an exception if not at least one site is loaded
-npm run grab -- --site=tv.blue.ch --days 3 --maxConnections=3
 # start the JavaScript server, EPG available at port=3000
 npm run serve &
+# Rest API throws an exception if not at least one site is loaded
+npm run grab -- --site=tv.blue.ch --days 3 --maxConnections=3
 # save EPG to cache
 if [ "$EPG_CACHED" = "true" ]
 then
-  sleep 30; \
   curl -o /iptv/iptv/src/data/epg_cached.xml localhost:3000/guide.xml; \
   chmod 666 /iptv/iptv/src/data/epg_cached.xml
 fi;
